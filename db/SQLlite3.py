430ad484e2f0b5ee48cd11cd09ceaec8e35f9644
loukikstudent <35080655+loukikstudent@users.noreply.github.com>
2020-09-05 17:12:52 +0530

    SQLLite3 class updated with CURD methods

--- db/SQLlite3.py
+++ db/SQLlite3.py
@@ -13,46 +13,73 @@ class RD_Manager:
         :rtype: object
         """
         self.db_name = db_name
-        # self.connection, self.cursor = self.create_connection()
-        # if not self.check_db():
+        # check table, if not exist then create
+        if not self.check_db():
+            self.create_table()
 
-    def create_connection(func):
+
+    def create_connection(func) :
         def wrapper(inst, *args, **kwargs):
-            cnn = sqlite3.connect("../"+inst.db_name)
-            logging.debug(f"Connetion Created for func: {func.__name__} with args: {args} and kwargs: {kwargs}")
-            cour = cnn.cursor()
-            cour.close()
-            r = func(inst,cnn, *args, **kwargs)
-            cnn.close()
+            cnn = sqlite3.connect(inst.db_name)
+            logging.debug(f"Connetion Created for func: {func.__name__}")
+            cur = cnn.cursor()
+            logging.debug(f"Cursor Created for func: {func.__name__} with args: {args} and kwargs: {kwargs}")
+            try:
+                r = func(inst,cur, *args, **kwargs)
+            except Exception as e:
+                logging.error(f"Function ({func._name__}) Execution failed")
+                logging.info("Initiating Roll Back")
+                cnn.rollback()
+                logging.info("Rollback completed")
+            else:
+                cnn.commit()
+            finally:
+                cur.close()
+                logging.debug(f"Cursor Close for func: {func.__name__} ")
+                cnn.close()
+                logging.debug(f"Connection Close for func: {func.__name__} ")
             return r
-        logging.debug(f"Connetion Close for func: {func.__name__} ")
         return wrapper
 
     @create_connection
-    def check_db(self, cnn, *args, **kwargs):
-        cour = cnn.cursor()
-        c = cour.execute('SELECT name FROM sqlite_master').fetchall()
-        cour.close()
-        print(c)
+    def check_db(self, cur,**kwargs):
+        """
 
-    # def create_connection(self):
-    #     con = sqlite3.connect(self.db_name)
-    #     c = con.cursor()
-    #     logging.debug("Connection Created")
-    #     return con, c
+        :type cur: sqlite3.connection.cursor
+        """
+        if cur.execute('SELECT name FROM sqlite_master where name = ?',('d3',)).fetchone():
+            logging.info("Table D3 not found!")
+            return True
+        return False
+
 
     @create_connection
-    def create_table(self):
+    def create_table(self,cur,**kwargs):
         query = "CREATE TABLE d3 (UUID BLOB NOT NULL UNIQUE PRIMARY KEY , KEY1 string NOT NULL, KEY2 string NOT NULL, KEY3 string NOT NULL,DATA string NOT NULL );"
-        self.cursor.execute(query)
+        cur.execute(query)
+        logging.info("D3 Table Connected")
 
-        logging.info("Prime Table Connected")
-
-    def add_d3(self, key1: str, key2: str, key3: str, data: str):
+    @create_connection
+    def add(self, cur,**kwargs):
         id = self.gen_uuid()
         query = "INSERT INTO d3 VALUES (?,?,?,?,?)"
-        self.c.execute(query, (id, key1, key2, key3, data,))
-        self.connection.commit()
+        cur.execute(query, (id, kwargs['key1'], kwargs['key2'], kwargs['key3'], kwargs['data'],))
+        logging.info(f"Entry updated for id: {id}")
+
+
+    @create_connection
+    def view(self, cur, **kwargs):
+        query = "SELECT data FROM d3 WHERE uuid=? and key1=? and key2=? and key3=?"
+        res = cur.execute(query,(kwargs['id'],kwargs['key1'],kwargs['key2'],kwargs['key3'],)).fetchone()
+        logging.info(f"Entry with id: {kwargs['id']} accessed")
+        return res
+
+    @create_connection
+    def delete(self, cur,**kwargs):
+        query = "DELETE from d3 where uuid = ?"
+        cur.execute(query,(kwargs['id'],))
+        logging.info(f"Entry with id: {kwargs['id']} from database")
+
 
     def gen_uuid(self):
         id = uuid.uuid4()
@@ -61,5 +88,4 @@ class RD_Manager:
 
 
 if __name__ == "__main__":
-    db = RD_Manager("test.sqlite3")
-    db.check_db()
+    db = RD_Manager("../test.sqlite3")
